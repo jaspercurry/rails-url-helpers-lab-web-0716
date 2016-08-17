b41d236975d776ec27ff3b9fd73101934942d7f5
jaspercurry <jaspercurry@gmail.com>
2016-08-17 15:52:03 -0400

    finished

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active=(arg)
+    self.status = arg
+  end
+
+  def active
+    self.status
+  end
+
+  def activate
+    self.active= !self.active
+    self.save
+  end
+
 end
